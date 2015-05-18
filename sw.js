9e34ea700fb960b9243f7e0862bc378339522aa7
Patrick Meenan <patmeenan@gmail.com>
2015-05-18 09:55:44 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -14,6 +14,6 @@ self.addEventListener('fetch', function(event) {
   event.respondWith(
     caches.match(event.request).then( function(response) {
       return response || fetch(event.request);
-    });
+    })
   );
 });
