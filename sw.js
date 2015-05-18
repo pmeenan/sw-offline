18cb7fca2c2730696a73bd07b718947f4bf12c18
Patrick Meenan <patmeenan@gmail.com>
2015-05-18 09:52:43 -0400

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,19 @@
+console.log("Service worker entry");
+
+self.addEventListener('install', function(e) {
+  console.log("Service worker installed");
+});
+
+self.addEventListener('activate', function(e) {
+  console.log("Service worker activated");
+});
+
+self.addEventListener('fetch', function(event) {
+  console.log('fetching');
+  console.log(event);
+  event.respondWith(
+    caches.match(event.request).then( function(response) {
+      return response || fetch(event.request);
+    });
+  );
+});
